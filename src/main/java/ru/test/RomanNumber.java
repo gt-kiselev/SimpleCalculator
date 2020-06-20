4f5d566617b467962233ebd4e4cef11ff4d7c8db
andrey <gt_kiselev@mail.ru>
2020-06-20 11:53:25 +0300

    rename Rome numbers

--- src/main/java/ru/test/RomanNumber.java
+++ src/main/java/ru/test/RomanNumber.java
@@ -8,18 +8,18 @@ import java.util.List;
 
 public class RomanNumber {
 
-    private static final List<String> romeNumbers = new ArrayList<>(Arrays.asList(
+    private static final List<String> romeTen = new ArrayList<>(Arrays.asList(
             "0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"
     ));
 
-    private static final List<String> romeDigits = new ArrayList<>(Arrays.asList(
+    private static final List<String> romeHundred = new ArrayList<>(Arrays.asList(
             "0", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"
     ));
 
     public static int toInt(String value) throws NotSupportedNumberException {
 
-        for (int i=0; i <= romeNumbers.size(); i++) {
-            if (romeNumbers.get(i).equals(value)) {
+        for (int i = 0; i <= romeTen.size(); i++) {
+            if (romeTen.get(i).equals(value)) {
                 return i;
             }
         }
@@ -29,13 +29,13 @@ public class RomanNumber {
     public static String toRome(int value) throws NotSupportedNumberException {
         try {
             if (value < 11) {
-                return romeNumbers.get(value);
+                return romeTen.get(value);
             } else if (value < 100) {
-                String firstDigit = romeDigits.get(value / 10);
-                String secondDigit = romeNumbers.get(value % 10);
+                String firstDigit = romeHundred.get(value / 10);
+                String secondDigit = romeTen.get(value % 10);
                 return firstDigit + secondDigit;
             } else if (value == 100) {
-                return romeDigits.get(10);
+                return romeHundred.get(10);
             } else {
                 throw new NotSupportedNumberException();
             }
